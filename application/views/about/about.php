960a345e9d5c5c428c91e80b2c0669f899e103f9
Eko Hardiyanto <ehardi19@gmail.com>
2019-04-10 12:21:04 +0700

    Added about page

---
+++ application/views/about/about.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="icon"  href="https://qsf.c7.quoracdn.net/-3-images.favicon.ico-26-ae77b637b1e7ed2c.ico">
+    <title>Tentang Qoura - Quora</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <style>
+      #search{
+        background-color: #f4f4f4;
+      }
+      #btn{
+        background-color:white;
+        border:1px solid #e5e5e5;
+        color:black;
+      }
+      #btn:hover{
+        border:1px soild black;
+      }
+      .row{
+        width:1400px;
+      }
+    </style>
+
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-sm" style="border-top:1px solid red; border-bottom:1px solid #cccccc; background-color:white; padding:0px; justify-content:center;">
+        <a class="navbar-brand" href="#">
+          <img src="https://2xawx0gmudy471po527lbxcd-wpengine.netdna-ssl.com/wp-content/uploads/2017/06/quora-604x400.png" alt="logo" style="width:70px; height=70px;">
+        </a>
+        <input id="search" class="form-control mr-sm-2" type="text" placeholder="  Cari pertanyaan, orang, dan topik" style="width:65%; padding:3px; ">
+        <button id="btn" class="btn btn-light" type="submit">MASUK</button>
+    </nav>
+
+    <form style="margin-top:100px; font-family:Arial;">
+      <div class="row">
+        <div class="col-sm-12">
+            <h1 style="text-align:center;">Mengapa Quora Ada</h1>
+        </div>
+
+      </div>
+      <div class="row">
+        <div class="col-sm-12">
+          <pre style="text-align:left; margin-left:25%; margin-top:15px; font-family:Arial;">
+          <b>  Mengapa Quora Ada</b>
+
+            Misi Quora adalah untuk membagikan dan mengembangkan pengetahuan dunia.
+            Sangat banyak pengetahuan yang mungkin akan bernilai bagi banyak orang yang pada
+            saat ini hanya tersedia untuk sedikit orang, baik itu karena masih tersimpan dalam
+            kepala, ataupun hanya dapat diakses oleh kelompok tertentu saja. Kami ingin
+            menghubungkan orang-orang yang memiliki pengetahuan dengan orang-orang yang
+            membutuhkannya, mengumpulkan orang-orang yang berbeda pandangan agar mereka
+            dapat saling mengerti satu sama lain dengan lebih baik, dan memberdayakan setiap
+            orang untuk membagikan pengetahuan mereka demi kepentingan seluruh dunia.
+
+            <b>Berkumpul di Sekeliling Pertanyaan</b>
+
+            Jantung Quora adalah pertanyaan. Pertanyaan <a href="#">yang memengaruhi dunia</a>, pertanyaan
+            <a href="#">yang menjelaskan peristiwa dunia terbaru</a> , <a href="#" style="text-decoration: none;">pertanyaan yang memandu keputusan
+            hidup yang penting</a>, dan pertanyaan<a href="#" style="text-decoration: none;"> yang memberikan wawasan mengapa orang lain
+            berpikir secara berbeda</a>.Quora adalah tempat di mana Anda dapat mengajukan
+            pertanyaan yang menjadi perhatian Anda dan mendapatkan jawaban yang luar biasa.
+
+            Quora hanya punya satu versi dari setiap pertanyaan. Tidak ada versi sayap kiri,
+            versi sayap kanan, versi barat, dan versi timur. Quora mengumpulkan orang-orang
+            dari berbagai belahan dunia berbeda untuk menjawab pertanyaan yang sama, di tempat
+            yang sama, dan untuk saling belajar satu sama lain. Kami ingin Quora menjadi
+            tempat untuk menyuarakan opini Anda karena Quora adalah tempat di mana debat
+            berlangsung. Kami ingin jawaban Quora menjadi jawaban definitif bagi semua orang
+            untuk selamanya.
+
+            <b>Memahami Dunia dan Orang-Orang di Dalamnya</b>
+
+            Quora memiliki konten yang akan membuat Anda merasa senang membaca. Quora
+            membantu Anda memahami bagaimana cara dunia bekerja, mengapa orang-orang
+            bertindak dengan cara yang mereka lakukan, dan apa yang dapat kita lakukan untuk
+            membuat dunia lebih baik. Quora menyediakan lini masa terpersonalisasi dari jawaban
+            berwawasan untuk pertanyaan yang tidak Anda sadari seharusnya Anda ajukan.
+
+            Jawaban Quora berasal dari orang-orang yang sangat memahami masalah dan memiliki
+            pengetahuan tangan pertama. Quora adalah tempat untuk membaca jawaban <a href="#" style="text-decoration: none;">Barack
+            Obama tentang perjanjian Iran</a>,<a href="#"> narapidana tentang kehidupan di penjara</a>,<a href="#" style="text-decoration: none;">ilmuwan
+            tentang pemanasan global</a>,<a href="#"> petugas polisi tentang cara mencegah terjadi pencurian</a>,
+            dan <a href="#">produser TV tentang bagaimana acara mereka dibuat</a>.Quora adalah tempat untuk
+            membaca jawaban dari orang-orang yang memberi inspirasi seperti <a href="#">Gloria Steinem</a>,
+            <a href="#">Stephen Fry</a>,<a href="#">Hillary Clinton</a>,<a href="#">Glenn Beck</a>,<a href="#">Sheryl Sandberg</a>,<a href="#">Vinod Khosla</a>, dan <a href="#" style="text-decoration: none;">Gillian
+            Anderson</a> yang secara langsung menjawab pertanyaan-pertanyaan yang paling
+            dinantikan oleh orang-orang. Quora adalah tempat di mana Anda dapat membaca
+            wawasan penting yang tidak pernah dibagikan di tempat lain mana pun, dari orang
+            yang tidak pernah bisa Anda jangkau dengan cara lain apa pun.
+
+
+          </pre>
+        </div>
+      </div>
+
+      <div class="row" style=" justify-content:center;">
+        <div class="col-sm-6">
+          <hr>
+        </div>
+      </div>
+    </form>
+
+  </body>
+</html>
