a8a4a1cad5c8d23952d8b7dcef99b607f89df984
Eko Hardiyanto <ehardi19@gmail.com>
2019-04-10 12:24:54 +0700

    Updated answer page

--- application/views/answer/answer.php
+++ application/views/answer/answer.php
@@ -102,15 +102,16 @@
             <hr>
             <small class="form-text text-muted" style="color:grey; margin-left:10px;">Pertanyaan ditambahkan</small>
             <a href="#" style="color:black; margin-left:10px; font-size:20px;">Pertanyaan User</a>
-            <small class="form-text text-muted"><a href="#" style="color:grey; margin-left:10px;">Jawab</a></small>            
+            <small class="form-text text-muted"><a href="#" style="color:grey; margin-left:10px;" data-toggle="collapse" data-target="#demo">Jawab</a></small>
           </div>
+        </div>
+        <div id="demo" class="collapse" style="margin-left:10px;">
+          <label for="comment">Comment:</label>
+          <textarea class="form-control" rows="5" id="comment"></textarea>
+          <button type="submit" class="btn btn-primary" name="button" style="margin-top:5px;">Selesai</button>
         </div>
       </div>
 
     </form>
-
-
-
-
 
 </body>
