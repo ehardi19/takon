2e45f105c6e709843c468a0e74c204c34c34b0b0
Eko Hardiyanto <ehardi19@gmail.com>
2019-04-14 14:38:37 +0700

    Clean the controller

--- application/controllers/User.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class User extends CI_Controller {
-    public function __construct() {
-        parent::__construct();
-        $this->load->model('User_model', 'users');
-    }
-}
